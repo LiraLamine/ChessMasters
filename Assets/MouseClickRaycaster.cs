518cc2d586dfa0857e81ae2e3fc4c087eb36184c
LiraLamine <bayon.arguu@gmail.com>
2017-04-07 16:21:36 -0400

    raycaster update

--- Assets/MouseClickRaycaster.cs
+++ Assets/MouseClickRaycaster.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class MouseClickRaycaster : MonoBehaviour {
 
+	Board boardscript;
 	// Update is called once per frame
 	void Update () {
 		if ( Input.GetMouseButtonDown (0)){ 
 			RaycastHit hit; 
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
 			if ( Physics.Raycast (ray,out hit,100.0f)) {
-				StartCoroutine(MoveMe(hit.transform));
-				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+				boardscript = hit.collider.gameObject.GetComponent<Board>();
+				Debug.Log("You selected the " + hit.collider.gameObject.name); // ensure you picked right object
 			}
 		}
 	}
-
-	void MoveMe(Transform objTr) {} //Temporary function that will call for movement
 }
